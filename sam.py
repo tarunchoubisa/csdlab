956aaf513c81d682a9b089d52d2ddbac04b99a93
Tarun Choubisa <choubisa.tarun@gmail.com>
2017-02-20 19:30:05 +0530

    video is not closing now in sam.py

--- sam.py
+++ sam.py
@@ -3,11 +3,11 @@ import numpy as np
 # hi how areyou
 #iam just okay
 # another hi comment
-c = cv2.VideoCapture(0)
-
-for x in range(20):c.read()
-
-s,frame = c.read()
+camera_id=0
+cap = cv2.VideoCapture(camera_id)
+for x in range(20):
+  cap.read()
+s,frame = cap.read()
 old_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 height,width,channels = frame.shape
@@ -51,28 +51,36 @@ while True:
 '''
 
 # Take first frame and find corners in it
-ret, old_frame = c.read()
+ret, old_frame = cap.read()
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 p0 = p0[:8]
-#print p0.shape
-
+print 'p0_shape' + str(p0.shape)
+print 'p0' + str(p0)
 #exit()
 
 while True:
-  s,f = c.read()
-  frame_gray = cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
+  s1,f1 = cap.read()
+  s,frame = cap.read()
+  frame_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
   p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
   old_gray = frame_gray.copy()
   
-  p1 = p1[st==1]
-  p0 = p0[st==1]
+  p1_new = p1[st==1]
+  p0_old = p0[st==1]
 
-  #print p0,p1
+  print 'p0' + str(p0)
+  print 'st' + str(st)
 
-  print st
-
-  cv2.imshow("frame",f)
+  #cv2.imshow("frame",frame)
+  for i,(new,old) in enumerate(zip(p1_new,p0_old)): # this syntax allows to take the elements of good_new  in variable new and old
+    #print enumerate(zip(good_new,good_old))
+    a,b = new.ravel() # good_new, it returns the flattened array, this is the coordinate corresponding to good_new points
+    c,d = old.ravel() # good_old, this is the old coordinate
+    cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+    cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+    img = cv2.add(frame,mask)
+    cv2.imshow('frame',img)
 
   if cv2.waitKey(1) & 0xff == ord('q'):break
 
